218299daf767cddb9e3e1b75ac70166275079f51
Jaspreet Kaur <jaspreet.kaur@digitalally.local>
2019-03-19 14:52:21 -0500

    We will try something new

---
+++ Hello.js
@@ -0,0 +1,7 @@
+console.log("This is ");
+console.log("I am finally getting ");
+console.log("This is so cool");
+console.log("I am finally getting the hang of it");
+console.log("We are the champions";
+console.log("We will keep trying");
+console.log("We have to keep doing our best in order to succeed"); 
